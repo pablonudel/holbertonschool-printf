c483b1e10af9eddf2828cf8017cee991b09a4ef3
pablonudel <pablonudel@gmail.com>
2024-11-25 15:56:19 +0100

    Testing the print_char function

--- _printf.c
+++ _printf.c
@@ -1,5 +1,15 @@
 #include "main.h"
 /**
+ * print_char - ...
+ * @args: va_list args
+ *
+ * Return: int
+ */
+int print_char(va_list args)
+{
+	return (write(1, va_arg(args, int), 1));
+}
+/**
  * _printf - ...
  * @format: char pointer
  *
@@ -7,5 +17,33 @@
  */
 int _printf(const char *format, ...)
 {
+	int i = 0, j = 0;
+	va_list args;
+
+	type_option_t options[] = {
+		{"c", print_char},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
 	
+	while (format[i] && format != NULL)
+	{
+		j = 0;
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			while (options[j].specifier && options[j].specifier[0] == format[i + 1])
+			{
+				options[j].specifier_len(args);
+				j++;
+			}
+		}
+		else
+			exit(98);
+		_putchar(format[i]);
+		i++;
+	}
+
+	va_end(args);
+
 }
