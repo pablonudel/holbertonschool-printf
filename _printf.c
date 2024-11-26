ff6b13fc03729f286bb94c006def971386ee88cd
pablonudel <pablonudel@gmail.com>
2024-11-26 15:24:02 +0100

    adding comments

--- _printf.c
+++ _printf.c
@@ -18,15 +18,18 @@ int _printf(const char *format, ...)
 	};
 
 	va_start(args, format);
-
 	while (format[i])
 	{
 		j = 0;
+
+		/* to match into the specifiers */
 		while (options[j].specifier)
 		{
 			if (format[i] == '%' && *options[j].specifier == format[i + 1])
 			{
+				/* each associated function should print the arg */
 				len += options[j].convert(args);
+				/* jump to next character after specifier */
 				i += 2;
 			}
 			j++;
