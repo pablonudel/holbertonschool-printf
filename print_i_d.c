bc37536009d5ba76f310b82892d089b5e052b933
pablonudel <pablonudel@gmail.com>
2024-11-28 21:47:49 +0100

    optimization of calculations and elimination of unnecessary variables

--- print_i_d.c
+++ print_i_d.c
@@ -8,38 +8,31 @@
 int print_i_d(va_list args)
 {
 	int arg = va_arg(args, int);
-	int digit, last_dig, pow = 1, len = 0;
-	unsigned int tmp_num = arg;
+	int pow = 1, len = 0;
+	unsigned int tmp_num;
 
 	if (arg < 0)
 	{
-		arg = arg * -1;
-		_putchar('-');
+		_putchat('-');
+		arg *= -1;
 		len++;
 	}
 
-	last_dig = arg % 10;
-	tmp_num = arg / 10;
+	tmp_num = arg;
 
 	if (tmp_num > 0)
 	{
-		while (tmp_num / 10 != 0)
-		{
-			tmp_num = tmp_num / 10;
-			pow = pow * 10;
-		}
+		while (tmp_num / pow >= 10)
+			pow *= 10;
 
-		tmp_num = arg / 10;
 		while (pow > 0)
 		{
-			digit = tmp_num / pow;
-			_putchar(digit + '0');
-			tmp_num = tmp_num - (digit * pow);
-			pow = pow / 10;
+			_putchar((tmp_num / pow) + '0');
+			tmp_num %= pow;
+			pow /= 10;
 			len++;
 		}
 	}
 
-	_putchar(last_dig + '0');
-	return (len + 1);
+	return (len);
 }
